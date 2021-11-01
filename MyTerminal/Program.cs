39d37aa55ec4608131db31061b885e4b691d3375
DrTau <drttau@gmail.com>
2021-11-01 17:31:59 +0300

    init commit

---
+++ MyTerminal/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyTerminal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
